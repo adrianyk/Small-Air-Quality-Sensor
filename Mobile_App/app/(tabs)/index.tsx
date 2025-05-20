11b4767d6044341a769048bc4cc9f8768a2d7f13
Adrian <adriankhoo6yk@gmail.com>
2025-05-20 19:48:31 +0100

    [akk121] customised home screen added background enabled vertical scrolling added icon at top of home screen/page NOTE: pp/(tabs)/index.tsx is the home screen

--- Mobile_App/app/(tabs)/index.tsx
+++ Mobile_App/app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
 // Home screen
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import {Link} from "expo-router"
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-500 font-bold">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: '100%', paddingBottom: 10 }}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
