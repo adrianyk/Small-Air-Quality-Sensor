f060a4484f9698351646293b947b42d6468dabf8
Ovidiu Costea <115478808+oc2222@users.noreply.github.com>
2025-06-12 16:13:24 +0100

    Update session.tsx
    Added list redirect

--- Mobile_App/app/(tabs)/session.tsx
+++ Mobile_App/app/(tabs)/session.tsx
@@ -1,12 +1,24 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+
 import ThemedView from "@/components/ThemedView";
 import ThemedText from "@/components/ThemedText";
 
 const Session = () => {
+    const router = useRouter();
+    
     return (
         <ThemedView className='flex-1 justify-center item-center'>
             <ThemedText className='font-bold text-[18px] text-center' title={false}>
                 Session Page
             </ThemedText>
+
+            <Link href='/session/history/list'>
+                <ThemedText className='text-center'>
+                    Session History
+                </ThemedText>
+            </Link>
+
         </ThemedView>
     )
 }
